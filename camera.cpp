fa371a3032a9a8b21b2a221a855abea8657408c7
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-06-21 14:21:04 +0900

    Camera更新 Class名の変更、Run関数を追加

--- camera.cpp
+++ camera.cpp
@@ -2,6 +2,7 @@
 #include"Window.h"
 #include"DirectXStruct.h"
 #include"DirectX.h"
+#include"Object.h"
 #include"Component.h"
 #include"Behaviour.h"
 #include"Tag.h"
@@ -11,36 +12,30 @@
 
 using namespace DirectX;
 
-std::shared_ptr<GameObject> CCamera::MainCamera;
+std::shared_ptr<Camera> Camera::MainCamera;
+std::list<std::shared_ptr<Camera>> Camera::CameraIndex;
 
-CCamera::CCamera()
+Camera::Camera()
+:
+	viewport({0L,0L,(long)D3DApp::GetScreenWidth(),(long)D3DApp::GetScreenHeight()}),
+	BGColor(Color::cyan()),
+	priority(1)
 {
-	
+	CameraIndex.push_back(std::make_shared<Camera>(*this));
 }
-
-
-void CCamera::Initialize()
+void Camera::Initialize()
 {
 	if (this->gameObject->CompareTag(TagManager::MainCamera))
-		MainCamera = std::make_shared<GameObject>(*this->gameObject);
-
-	m_Viewport.left = 0;
-	m_Viewport.top = 0;
-	m_Viewport.right = D3DApp::GetScreenWidth();
-	m_Viewport.bottom = D3DApp::GetScreenHeight();
+		MainCamera = std::make_shared<Camera>(*this);
+	
 }
 
-void CCamera::Finalize()
+void Camera::Update()
 {
 	
 }
 
-void CCamera::Update()
-{
-	gameObject->transform.rotation.y += XM_PI / 180 * 0.5;
-}
-
-void CCamera::Render()
+void Camera::Render()
 {
 	XMMATRIX	m_ViewMatrix;
 	XMMATRIX	m_InvViewMatrix;
@@ -48,12 +43,51 @@ void CCamera::Render()
 
 	// ビューポート設定
 	D3D11_VIEWPORT dxViewport;
-	dxViewport.Width = (float)(m_Viewport.right - m_Viewport.left);
-	dxViewport.Height = (float)(m_Viewport.bottom - m_Viewport.top);
+	dxViewport.Width = (float)(viewport.right - viewport.left);
+	dxViewport.Height = (float)(viewport.bottom - viewport.top);
 	dxViewport.MinDepth = 0.0f;
 	dxViewport.MaxDepth = 1.0f;
-	dxViewport.TopLeftX = (float)m_Viewport.left;
-	dxViewport.TopLeftY = (float)m_Viewport.top;
+	dxViewport.TopLeftX = (float)viewport.left;
+	dxViewport.TopLeftY = (float)viewport.top;
+
+	D3DApp::GetDeviceContext()->RSSetViewports(1, &dxViewport);
+
+
+	// ビューマトリクス設定
+	m_InvViewMatrix = gameObject->transform.MatrixRotationRollPitchYaw();
+	m_InvViewMatrix *= gameObject->transform.MatrixTranslation();
+
+	XMVECTOR det;
+	m_ViewMatrix = XMMatrixInverse(&det, m_InvViewMatrix);
+
+	D3DApp::Renderer::SetViewMatrix(&m_ViewMatrix);
+
+	// プロジェクションマトリクス設定
+	m_ProjectionMatrix = XMMatrixPerspectiveFovLH(1.0f, dxViewport.Width / dxViewport.Height, 1.0f, 1000.0f);
+
+	D3DApp::Renderer::SetProjectionMatrix(&m_ProjectionMatrix);
+}
+
+void Camera::Finalize()
+{
+	
+}
+
+
+void Camera::Run()
+{
+	XMMATRIX	m_ViewMatrix;
+	XMMATRIX	m_InvViewMatrix;
+	XMMATRIX	m_ProjectionMatrix;
+
+	// ビューポート設定
+	D3D11_VIEWPORT dxViewport;
+	dxViewport.Width = (float)(viewport.right - viewport.left);
+	dxViewport.Height = (float)(viewport.bottom - viewport.top);
+	dxViewport.MinDepth = 0.0f;
+	dxViewport.MaxDepth = 1.0f;
+	dxViewport.TopLeftX = (float)viewport.left;
+	dxViewport.TopLeftY = (float)viewport.top;
 
 	D3DApp::GetDeviceContext()->RSSetViewports(1, &dxViewport);
 
@@ -73,8 +107,21 @@ void CCamera::Render()
 	m_ProjectionMatrix = XMMatrixPerspectiveFovLH(1.0f, dxViewport.Width / dxViewport.Height, 1.0f, 1000.0f);
 
 	D3DApp::Renderer::SetProjectionMatrix(&m_ProjectionMatrix);
-
-
-
 }
 
+void Camera::SetViewPort(long x, long y, long w, long h)
+{
+	viewport.left = x;
+	viewport.top = y;
+	viewport.right = x + w;
+	viewport.bottom = y + h;
+}
+
+void DirectX::Camera::Destroy()
+{
+	for (std::shared_ptr<Camera> camera : CameraIndex)
+	{
+		if (camera._Get() != this) continue;
+		CameraIndex.remove(camera);
+	}
+}
--- camera.h
+++ camera.h
@@ -2,18 +2,30 @@
 
 namespace DirectX
 {
-	class CCamera :public Behaviour
+	class Camera :public Behaviour
 	{
 	private:
-		static std::shared_ptr<GameObject> MainCamera;
+		static std::shared_ptr<Camera> MainCamera;
+		static std::list<std::shared_ptr<Camera>> CameraIndex;
 	private:
-		RECT m_Viewport;
+		RECT viewport;
+		Color BGColor;	//”wŒiF
+		int priority;
 	public:
-		CCamera();
+		Camera();
+		virtual ~Camera() {};
 	public:
+		//Component
 		void Initialize()override;
 		void Update()override;
 		void Render()override;
 		void Finalize()override;
+	public:
+		//Method
+		virtual void Run();	//•`‰æ
+		void SetViewPort(long x, long y, long w, long h);
+		void SetPriority(int priority) { this->priority = priority; };
+		void SetBGColor(Color color) { this->BGColor = color; };
+		void Destroy() override;
 	};
 }
