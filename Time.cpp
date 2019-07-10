c029279cf8f9c9d407db2a4b8b2a1d190594e34f
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-07-10 09:43:31 +0900

    null文 秒カウンタ実装

--- Time.cpp
+++ Time.cpp
@@ -1,4 +1,5 @@
 #include"main.h"
+#include"GUI_ImGui.h"
 #include "Time.h"
 
 #define SET_SECOND_TIME (0.001f)
@@ -15,6 +16,7 @@ TimeManager::TimeManager(unsigned long fps)
 	dwStartTime(0),
 	fps(1000/fps),
 	dwCurrentTime(0),
+	FpsCounter(0),
 	dwExecLastTime(0),
 	dwDeltaTime(0),
 	dwFixedTime(1000/(fps/2)),
@@ -53,6 +55,7 @@ bool TimeManager::IsUpdate()
 	if (frameTime < pInstance->fps) return false;
 	pInstance->dwDeltaTime = frameTime;
 	pInstance->dwExecLastTime = pInstance->dwCurrentTime;
+	pInstance->FpsCounter++;
 
 	return true;
 }
@@ -73,43 +76,71 @@ bool TimeManager::IsFixedUpdate()
 void TimeManager::Update()
 {
 	if (!pInstance) return;
+	
+	if ((pInstance->dwCurrentTime - pInstance->dwFpsLastTime) >= 1000){
+		pInstance->dwFpsLastTime = pInstance->dwCurrentTime;
+		pInstance->Fps = pInstance->FpsCounter;
+		pInstance->FpsCounter = 0;
+	}
+
 	pInstance->dwCurrentTime = timeGetTime();
+}
+
+void DirectX::TimeManager::DebugGUI_Time()
+{
+	ImGui::Begin("Time");
+	ImGui::Text("FPS:%d",pInstance->Fps);
+	ImGui::Text("deltaTime %f", Time::Get_DeltaTime());
+	ImGui::Text("fixedTime %f", Time::Get_FixedDeltaTime());
+	ImGui::End();
 }
 
 //--- Time --------------------------------------------------------------------
 
 float Time::Get_DeltaTime()
 {
+	if (TimeManager::pInstance == nullptr) return 0.0f;
 	return TimeManager::pInstance->dwDeltaTime * TimeManager::pInstance->TimeScale * SET_SECOND_TIME;	
 }
 
 float Time::Get_FixedTime()
 {
+	if (TimeManager::pInstance == nullptr) return 0.0f;
 	return TimeManager::pInstance->dwFixedTime * SET_SECOND_TIME;
 }
 
 float Time::Get_Time()
 {
+	if (TimeManager::pInstance == nullptr) return 0.0f;
 	return (TimeManager::pInstance->dwStartTime - timeGetTime()) * SET_SECOND_TIME;
 }
 
 float Time::Get_TimeScale()
 {
+	if (TimeManager::pInstance == nullptr) return 0.0f;
 	return TimeManager::pInstance->TimeScale;
 }
 
 float Time::Get_FixedDeltaTime()
 {
+	if (TimeManager::pInstance == nullptr) return 0.0f;
 	return TimeManager::pInstance->dwFixedDeltaTime * SET_SECOND_TIME;
+}
+
+int DirectX::Time::Get_SecGameTime()
+{
+	if (TimeManager::pInstance == nullptr) return 0;
+	return (TimeManager::pInstance->dwCurrentTime - TimeManager::pInstance->dwStartTime) / 1000;
 }
 
 void Time::Set_TimeScale(float TimeScale)
 {
+	if (TimeManager::pInstance == nullptr) return;
 	TimeManager::pInstance->TimeScale = TimeScale;
 }
 
 void Time::Set_FixedScale(DWORD fixedtime)
 {
+	if (TimeManager::pInstance == nullptr) return;
 	TimeManager::pInstance->dwFixedTime = fixedtime;
 }
-
--- Time.h
+++ Time.h
@@ -2,6 +2,7 @@
 
 namespace DirectX
 {
+	
 	class Time
 	{
 		friend class TimeManager;
@@ -14,6 +15,7 @@ namespace DirectX
 		static float Get_Time();
 		static float Get_TimeScale();
 		static float Get_FixedDeltaTime();
+		static int Get_SecGameTime();
 	public:
 		static void Set_TimeScale(float TimeScale);
 		static void Set_FixedScale(DWORD fixedtime);
@@ -27,17 +29,21 @@ namespace DirectX
 	private:
 		float TimeScale;		//時間倍率
 		DWORD dwStartTime;		//開始時間
-		const DWORD fps;
+		const DWORD fps;		//目標FPS
 		DWORD dwCurrentTime;	//現在
 
+		DWORD dwFpsLastTime;	//前FPSカウンタ更新時間
+		int FpsCounter;			//FPSカウンタ
+		int Fps;				//実FPS
+
 		//Main
-		DWORD dwExecLastTime;
-		DWORD dwDeltaTime;
+		DWORD dwExecLastTime;	//最終更新時間
+		DWORD dwDeltaTime;		//Update間隔時間
 
 		//Fixed
-		DWORD dwFixedTime;
-		DWORD dwFixedLastTime;
-		DWORD dwFixedDeltaTime;
+		DWORD dwFixedTime;		//一定時間
+		DWORD dwFixedLastTime;	//FixedUpdate最終更新時間
+		DWORD dwFixedDeltaTime;	//FixedUpdate間隔時間
 	private:
 		TimeManager(unsigned long fps);
 		~TimeManager() {};
@@ -48,5 +54,6 @@ namespace DirectX
 		static bool IsUpdate();
 		static bool IsFixedUpdate();
 		static void Update();
+		static void DebugGUI_Time();
 	};
 }
