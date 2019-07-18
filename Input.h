a7977edf03256c8be5da0d9ab665c05f294f5484
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-07-18 16:56:15 +0900

    Mouseなどの入力を設定

--- Input.h
+++ Input.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define DIRECTINPUT_VERSION 0x0800
+#include<dinput.h>
+
 namespace DirectX
 {
 	class Input
@@ -7,15 +10,42 @@ namespace DirectX
 	private:
 		static BYTE m_OldKeyState[256];
 		static BYTE m_KeyState[256];
+		static POINT m_MousePos;
+		static bool IsCursorLoop;
 
+		static LPDIRECTINPUT8 m_pInput;
+		static LPDIRECTINPUTDEVICE8 m_pDevMouse;
+		static DIMOUSESTATE2 m_MouseState2;
 	public:
 		static void Init();
 		static void Uninit();
 		static void Update();
 
+		//à¯êî:VK_@@@ '@'
 		static bool GetKeyPress(BYTE KeyCode);
+		static bool GetKeyCodePress(BYTE keyCode);
 		static bool GetKeyTrigger(BYTE KeyCode);
 		static bool GetKeyUp(BYTE KeyCode);
 
+	public:
+		static void DebugGUI();
+	public:
+		//É}ÉEÉX
+		class Mouse
+		{
+		public:
+			static void SetMouseShow();
+			static void SetMouseHide();
+			static void SetScreenLoop(bool IsLoop);
+		public:
+			static float GetMouseX();
+			static float GetMouseY();
+			static bool IsLeftDown();
+			static bool IsRightDown();
+			static bool IsWheelDown();
+			static float GetAccelerationX();
+			static float GetAccelerationY();
+			static float GetAccelerationZ();
+		};
 	};
 }
