9309b983c64e5652fbf83d11e370d7968aaf772f
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-07-18 17:01:18 +0900

    SceneManagerからUnLoadなど削除・仕様変更

--- SceneManager.cpp
+++ SceneManager.cpp
@@ -14,95 +14,107 @@
 #include"Behaviour.h"
 #include"camera.h"
 #include"SceneManager.h"
+#include"Collision.h"
 
 using namespace DirectX;
 
-SceneManager* SceneManager::pInstance = NULL;
+SceneManager* SceneManager::pInstance = nullptr;
 
+//Sceneの保管庫
 std::list<std::shared_ptr<Scene>> SceneManager::pSceneIndex;
+//現在のScene
 std::weak_ptr<Scene> SceneManager::pActiveScene;
+//次のScene
+std::weak_ptr<Scene> SceneManager::pNextScene;
+//遷移が有効か
+bool SceneManager::IsChangeScene = false;
 
 //--- SceneManager ------------------------------------------------------------
-
 SceneManager::SceneManager()
 {
 	pSceneIndex.clear();
 }
-
 SceneManager::~SceneManager()
 {
 	if (!pActiveScene.expired())
 		pActiveScene.reset();
+	if (!pNextScene.expired())
+		pNextScene.reset();
 
 	if (pSceneIndex.size() != 0)
 		pSceneIndex.clear();
 }
 
 //--- Singleton Method ----------------------------------------------
-
+//インスタンス生成
 void SceneManager::Create()
 {
-	if (pInstance != NULL) return;
+	if (pInstance != nullptr) return;
 	pInstance = new SceneManager();
 }
-
+//インスタンス削除
 void SceneManager::Destroy()
 {
-	if (pInstance == NULL) return;
+	if (pInstance == nullptr) return;
 	delete pInstance;
 }
 
 //--- Scene Method --------------------------------------------------
 
+//Scene名が一致していれば読み込み
 void SceneManager::LoadScene(std::string SceneName)
 {
+	//Index検索
 	for (auto wp_Scene : pSceneIndex)
-	{
-		if (wp_Scene->name != SceneName) continue;
-		if (wp_Scene->IsLoaded == true) break;
-		pActiveScene = wp_Scene;
-		pActiveScene.lock();
-		wp_Scene->IsLoaded = true;
-		break;
+		if (wp_Scene->name == SceneName){
+			LoadScene(wp_Scene);
+			return;
+		}
+}
+
+//Scene読み込み
+void DirectX::SceneManager::LoadScene(std::weak_ptr<Scene> scene)
+{
+	if(pActiveScene.expired()){
+		AttachActiveScene(scene);
+		return;
 	}
+	//次のSceneへ登録
+	SetIsChangeScene(scene);
 }
 
-void SceneManager::UnLoadScene(std::string SceneName)
-{
-	if (pActiveScene._Get()->name != SceneName) return;
-	pActiveScene.reset();
-}
-
-Scene* SceneManager::GetScene(std::string SceneNamae)
-{
-	for (auto wp_Scene : pSceneIndex)
-		if (wp_Scene->name == SceneNamae) return &(*wp_Scene);
-	return NULL;
-}
-
+//ActiveSceneのComponentをへメッセージを送信する
 void DirectX::SceneManager::RunActiveScene(Component::Message message)
 {
-	if (pActiveScene.expired()) return;
 	for (std::shared_ptr<GameObject> gameObject : pActiveScene.lock()->GameObjectIndex)
 		gameObject->RunComponent(message);
 }
 
+//ActiveScene内の当たり判定を更新
 void DirectX::SceneManager::OnTriggerUpdate()
 {
-	if (pActiveScene.expired()) return;
 	for (std::shared_ptr<GameObject> gameObject : pActiveScene.lock()->GameObjectIndex){
-		
+		if (!gameObject.get()->IsActive)continue;
+		std::weak_ptr<Collider> collider = gameObject.get()->GetComponent<Collider>();
+		if (collider.expired())continue;
+		//Triggerではない
+		if (!collider.lock()->GetEnable() || !collider.lock()->GetTrigger()) continue;
+		//相手
+		for(std::shared_ptr<GameObject> otherObject:pActiveScene.lock()->GameObjectIndex){
+			if (gameObject == otherObject) continue;
+		}
 	}
 }
 
+//ActiveScene内の物理当たり判定を更新
 void DirectX::SceneManager::OnCollisionUpdate()
 {
+
 }
 
+//ActiveScene内オブジェクトのImGUI表示
 void DirectX::SceneManager::DebugGUI_ActiveScene()
 {
-	if (pActiveScene.expired()) return;
-	
 	std::string label = "Scene:";
 	label += pActiveScene.lock()->name;
 	ImGui::Begin(label.c_str());
@@ -131,19 +143,72 @@ void DirectX::SceneManager::DebugGUI_ActiveScene()
 	ImGui::End();
 }
 
+//削除指定されたオブジェクトを削除
 void DirectX::SceneManager::CleanUp()
 {
-	if (pActiveScene.expired()) return;
+	//CeanUpしなくていい
 	if (!pActiveScene._Get()->IsCeanUp) return;
+	//削除
 	pActiveScene._Get()->GameObjectIndex.remove_if([](std::shared_ptr<GameObject> gameObject) { return gameObject->IsDestroy; });
+	//CeanUp完了
 	pActiveScene._Get()->IsCeanUp = false;
+}
+
+//Scene遷移
+void DirectX::SceneManager::ChangeScene()
+{
+	//遷移フラグが有効
+	if (!IsChangeScene) return;
+	//ActiveSceneを解除
+	DetachActiveScene();
+	//次のSceneをActiveSceneへ変更
+	AttachActiveScene(pNextScene);
+	IsChangeScene = false;
+}
+
+//ActiveSceneの取得
+std::weak_ptr<Scene> DirectX::SceneManager::GetActiveScene()
+{
+	return pActiveScene;
+}
+
+//Scene名からSceneを取得
+std::weak_ptr<Scene> DirectX::SceneManager::GetSceneByName(std::string SceneName)
+{
+	for (auto wp_Scene : pSceneIndex)
+		if (wp_Scene->name == SceneName)
+			return wp_Scene;
+	return std::weak_ptr<Scene>();
+}
+
+//ActiveSceneを設定
+void DirectX::SceneManager::AttachActiveScene(std::weak_ptr<Scene> scene)
+{
+	pActiveScene.reset();
+	pActiveScene = scene;
+	scene.lock()->IsLoaded = true;
+	scene.lock()->IsCeanUp = false;
+	scene.lock()->Load();
+	//初期化処理
+	RunActiveScene(Component::Initialize);
+}
+
+//ActiveSceneを破棄
+void DirectX::SceneManager::DetachActiveScene()
+{
+	pActiveScene.lock()->IsLoaded = false;
+	pActiveScene.lock()->IsCeanUp = false;
+	//終了処理
+	RunActiveScene(Component::Finalize);
+	pActiveScene.lock()->GameObjectIndex.clear();
+	pActiveScene.reset();
 }
 
 //--- Scene -------------------------------------------------------------------
 
 Scene::Scene(std::string name):name(name)
 {
-
+	GameObjectIndex.clear();
 }
 
 Scene::~Scene()
@@ -151,6 +216,7 @@ Scene::~Scene()
 	GameObjectIndex.clear();
 }
 
+//GameObjectをTag指定で追加
 GameObject* Scene::AddSceneObject(std::string name,TagName tag)
 {
 	std::shared_ptr<GameObject> object = std::shared_ptr<GameObject>(new GameObject(name,this,tag));
@@ -160,9 +226,9 @@ GameObject* Scene::AddSceneObject(std::string name,TagName tag)
 	return object.get();
 }
 
+//GameObjectをDefaultTagで指定
 GameObject* Scene::AddSceneObject(std::string name)
 {
 	return AddSceneObject(name, TagName::Default);
 }
-
 
--- SceneManager.h
+++ SceneManager.h
@@ -1,43 +1,85 @@
 #pragma once
 
+
 namespace DirectX
 {
 	class Scene;
 	class GameObject;
 
+	/*
+	設計：
+		ActiveSceneは必ず存在する(存在しない場合はnullptrとして例外を起こすべき)
+	*/
+
+	//=== Scene管理 ===========================================================
 	class SceneManager
 	{
 	private:
 		static SceneManager* pInstance;
+		//Sceneの保管庫
 		static std::list<std::shared_ptr<Scene>> pSceneIndex;
+		//現在のScene
 		static std::weak_ptr<Scene> pActiveScene;
+		//次のScene
+		static std::weak_ptr<Scene> pNextScene;
+		static bool IsChangeScene;
 	private:
 		SceneManager();
 		~SceneManager();
+	//--- Singleton -------------------------------------------------
 	public:
-		//Singleton
-		static void Create();
-		static void Destroy();
-		//Scene
-		static void LoadScene(std::string SceneName);
-		static void UnLoadScene(std::string SceneName);
-		template<typename Type> static void CreateScene()
+		static void Create();			//インスタンス生成
+		static void Destroy();			//インスタンス削除
+
+	//--- Scene -----------------------------------------------------
+	public:
+		//Sceneの生成
+		template<typename Type>
+		static void CreateScene()
 		{
 			std::shared_ptr<Scene> AddScene = std::shared_ptr<Scene>(new Type());
 			pSceneIndex.push_back(AddScene);
 			AddScene->self = AddScene;
 			return;
 		};
-		static Scene* GetScene(std::string SceneName);
-		static void RunActiveScene(Component::Message message);
-		static void OnTriggerUpdate();
-		static void OnCollisionUpdate();
-		static void DebugGUI_ActiveScene();
 	public:
+		//Sceneの読み込み
+		static void LoadScene(std::string SceneName);
+		//Sceneの読み込み
+		static void LoadScene(std::weak_ptr<Scene> scene);	
+		//ActiveSceneへメッセージを送信
+		static void RunActiveScene(Component::Message message);
+		//ActiveSceneの当たり判定を更新
+		static void OnTriggerUpdate();
+		//ActiveSceneの物理当たり判定を更新
+		static void OnCollisionUpdate();
+		//ActiveScene内オブジェクトのImGUI表示
+		static void DebugGUI_ActiveScene();
+		//ActiveScene内で削除指定されたオブジェクトを削除
 		static void CleanUp();
+		//ActiveSceneとNextSceneを切り替え
+		static void ChangeScene();
+		//GetActiveScene
+		static std::weak_ptr<Scene>GetActiveScene();
+		//GetSceneByName
+		static std::weak_ptr<Scene>GetSceneByName(std::string SceneName);
+
+	//--- 内部処理 --------------------------------------------------
+	private:
+		//SetIsChangeScene
+		static void SetIsChangeScene(std::weak_ptr<Scene> scene)
+		{
+			pNextScene = scene;
+			IsChangeScene = true;
+		}
+		//SetActiveScene
+		static void AttachActiveScene(std::weak_ptr<Scene> scene);
+		//DitachActiveScene
+		static void DetachActiveScene();
 	};
 
-	class Scene 
+	//=== Scene ==================================================================
+	class Scene
 	{
 		friend class SceneManager;
 	private:
@@ -51,10 +93,12 @@ namespace DirectX
 		Scene(std::string name);
 		virtual ~Scene();
 	public:
+		//Sceneが読み込まれた時
+		virtual void Load() = 0;
+		//ゲームオブジェクトの生成
 		GameObject* AddSceneObject(std::string name, TagName tag);
 		GameObject* AddSceneObject(std::string name);
+		//ゲームオブジェクトの削除処理を予約
 		void SetIsCeanUp(bool IsEnable) { this->IsCeanUp = IsEnable; };
-		
 	};
-
 }
