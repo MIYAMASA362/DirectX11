08ed6569f8f8d5dc5e12fd8dbaed4778606a24f0
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-07-25 17:14:56 +0900

    Module更新

--- Engine/Module/Module.h
+++ Engine/Module/Module.h
@@ -1,1 +1,32 @@
 #pragma once
+
+//DirectX
+#include"DirectX\DirectXStruct.h"
+#include"DirectX\DirectX.h"
+
+//Device
+#include"Input\Input.h"
+
+//Asset
+#include"Audio\Audio.h"
+#include"Texture\texture.h"
+
+//Component
+#include"Object\Object.h"
+#include"Component\Component.h"
+
+#include"Transform\Transform.h"
+#include"Behaviour\Behaviour.h"
+#include"Tag\Tag.h"
+#include"GameObject\GameObject.h"
+
+//Component Module
+#include"Renderer\Renderer.h"
+#include"Field\field.h"
+#include"Model\model.h"
+#include"Camera\camera.h"
+#include"Collision\Collision.h"
+
+#include"Texture\TextureManager.h"
+#include"Model\ModelManager.h"
+#include"Audio\AudioManager.h"
