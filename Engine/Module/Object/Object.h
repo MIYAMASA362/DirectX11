c437ceae60887e9ebc69dfb72b04a4878f4e5e15
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-08-27 18:08:12 +0900

    Object

--- Engine/Module/Object/Object.h
+++ Engine/Module/Object/Object.h
@@ -9,21 +9,16 @@ namespace DirectX
 	*/
 	class Object
 	{
-	protected:
-		
-		unsigned int id;
-		std::string name;
 	public:
-		Object() :name(typeid(*this).name()),id(0){};
-		Object(std::string name) :name(name),id(0){};
+		Object(){};
 		virtual ~Object() 
 		{ 
 			OnDestroy(); 
 		};
 	public:
 		virtual void OnDestroy() {};
-		std::string ObjectName(){
-			return name;
-		}
+		std::string ObjectTypeName() {
+			return typeid(*this).name();
+		};
 	};
 }
