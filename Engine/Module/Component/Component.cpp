a50bcaa42867f24365eb686e52f17b273347e7ee
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-08-27 18:05:05 +0900

    Component

--- Engine/Module/Component/Component.cpp
+++ Engine/Module/Component/Component.cpp
@@ -3,9 +3,20 @@
 #include<string>
 #include"../Object/Object.h"
 #include"Component.h"
+
+#include"Module\IMGUI\GUI_ImGui.h"
+
 using namespace DirectX;
 
 void Component::SetEnable(bool isEnable)
 {
 	this->IsEnable = isEnable;
+}
+
+void DirectX::Component::DebugImGui()
+{
+	if (ImGui::TreeNode(this->name.c_str())) {
+		ImGui::TreePop();
+	}
+	
 }
--- Engine/Module/Component/Component.h
+++ Engine/Module/Component/Component.h
@@ -5,18 +5,25 @@ namespace DirectX
 	class GameObject;
 	class Transform;
 	class Behaviour;
+	class Collision;
+	class Collider;
+
 
 	/**
-	@virtual
+	@override
 	+ Destroy()
 	+ OnDestroy()
 	+ OnComponent()
 	+ SendBehaviourMessage(Message message)
+	+ GetComponentName()
+	+ GetType()
 	*/
 	class Component:public Object
 	{
 		friend GameObject;
+	//--- Attribute -------------------------------------------------
 	public:
+		//BehaviourMessage
 		enum Message
 		{
 			Initialize,
@@ -26,20 +33,37 @@ namespace DirectX
 			Finalize,
 		};
 	private:
-		const int priority;
+		const std::string name;					//Component名
 	protected:
-		bool IsEnable = true;
+		bool IsEnable = true;					//Componentの有効・無効
 	public:
-		std::weak_ptr<GameObject> gameObject;
-		std::weak_ptr<Transform> transform;
+		std::weak_ptr<GameObject> gameObject;	//アタッチされているGameObjectへの参照
+		std::weak_ptr<Transform> transform;		//アタッチされているGameObjectのTransformへの参照
+	//--- Constrcutor/Destrcutor ------------------------------------
 	public:
-		Component(int priority) :priority(priority){};
-		Component() :Component(0) {};
+		Component(std::string name):name(name){};
 		virtual ~Component() {};
+	//--- Method ----------------------------------------------------
 	public:
 		void SetEnable(bool isEnable);
 		bool GetEnable() { return IsEnable; };
+	public:
+		//Messageを送る
 		virtual void SendBehaviourMessage(Message message) {};
+		//Componentがアタッチされたら呼ばれる
 		virtual void OnComponent() {};
+		//Component名取得
+		std::string GetComponentName() { return name; };
+		virtual void OnDestroy() override {};
+		virtual const std::type_info& GetType() = 0;
+		virtual void DebugImGui();
+	//当たり判定系Message
+	public:
+		virtual void OnTriggerEnter(std::weak_ptr<Collider> other) {};
+		virtual void OnTriggerExit(std::weak_ptr<Collider> other) {};
+		virtual void OnTriggerStay(std::weak_ptr<Collider> other) {};
+		virtual void OnCollisionEnter(std::weak_ptr<Collision> collision) {};
+		virtual void OnCollisionExit(std::weak_ptr<Collision> collision) {};
+		virtual void OnCollisionStay(std::weak_ptr<Collision> collision) {};
 	};
 }
