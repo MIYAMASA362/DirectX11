567267bd58e9bbc9c6d96c6a0a69a1d6a58e164b
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-01 02:23:46 +0900

    SceneManagerとSceneのfriendを解消して整理

--- Engine/Module/Scene/SceneManager.cpp
+++ Engine/Module/Scene/SceneManager.cpp
@@ -1,4 +1,5 @@
 #include<List>
+#include<map>
 #include<typeinfo>
 #include<string>
 #include<memory>
@@ -24,6 +25,7 @@
 
 #include"Module\UI\UI.h"
 
+#include"Module\SystemManager\SystemManager.h"
 #include"SceneManager.h"
 #include"Module\Physics\Collision.h"
 
@@ -31,10 +33,8 @@
 
 using namespace DirectX;
 
-SceneManager* SceneManager::pInstance = nullptr;
-
 //Sceneの保管庫
-std::list<std::shared_ptr<Scene>> SceneManager::pSceneIndex;
+std::map<std::string, std::shared_ptr<Scene>> SceneManager::pSceneDictionary;
 //現在のScene
 std::weak_ptr<Scene> SceneManager::pActiveScene;
 //次のScene
@@ -42,34 +42,15 @@ std::weak_ptr<Scene> SceneManager::pNextScene;
 //遷移が有効か
 bool SceneManager::IsChangeScene = false;
 
-//--- SceneManager ------------------------------------------------------------
-SceneManager::SceneManager()
-{
-	pSceneIndex.clear();
-}
-SceneManager::~SceneManager()
+//インスタンス削除
+void SceneManager::Destroy()
 {
 	if (!pActiveScene.expired())
 		pActiveScene.reset();
 	if (!pNextScene.expired())
 		pNextScene.reset();
 
-	if (pSceneIndex.size() != 0)
-		pSceneIndex.clear();
-}
-
-//--- Singleton Method ----------------------------------------------
-//インスタンス生成
-void SceneManager::Create()
-{
-	if (pInstance != nullptr) return;
-	pInstance = new SceneManager();
-}
-//インスタンス削除
-void SceneManager::Destroy()
-{
-	if (pInstance == nullptr) return;
-	delete pInstance;
+	pSceneDictionary.clear();
 }
 
 //--- Scene Method --------------------------------------------------
@@ -77,17 +58,13 @@ void SceneManager::Destroy()
 //Scene名が一致していれば読み込み
 void SceneManager::LoadScene(std::string SceneName)
 {
-	//Index検索
-	for (auto wp_Scene : pSceneIndex)
-		if (wp_Scene->name == SceneName){
-			LoadScene(wp_Scene);
-			return;
-		}
+	LoadScene(pSceneDictionary.at(SceneName));
 }
 
 //Scene読み込み
 void DirectX::SceneManager::LoadScene(std::weak_ptr<Scene> scene)
 {
+	//ActiveSceneが無い
 	if(pActiveScene.expired()){
 		AttachActiveScene(scene);
 		return;
@@ -97,50 +74,9 @@ void DirectX::SceneManager::LoadScene(std::weak_ptr<Scene> scene)
 }
 
 //ActiveSceneのComponentをへメッセージを送信する
-void DirectX::SceneManager::RunActiveScene(Component::Message message,BYTE option)
+void DirectX::SceneManager::RunActiveScene(Component::Message message)
 {
-	//Messageを送るオブジェクト群
-	std::list<std::shared_ptr<GameObject>> RunObjectIndex;
-
-	//全てのGameObjectに更新をMessageを送信
-	if (option == SCENE_OPTION_UPDATE_DEFAULT || option == SCENE_OPTION_UPDATE_ALL)
-		RunObjectIndex = pActiveScene.lock()->GameObjectIndex;
-	//抽出処理
-	else {
-		//抽出条件
-		bool IsActive,	//GameObjectがActiveであるものを抽出
-			 IsParent;	//GameObjectが親であるものを抽出
-
-		//フラグ計算
-		{
-			IsActive = { SCENE_OPTION_UPDATE_ACTIVE == (option & SCENE_OPTION_UPDATE_ACTIVE) };
-			IsParent = { SCENE_OPTION_UPDATE_PARENT == (option & SCENE_OPTION_UPDATE_PARENT) };
-		}
-
-		//抽出
-		for (auto gameObject : pActiveScene.lock()->GameObjectIndex) {
-			if (IsActive)
-				if (!gameObject->IsActive)
-					continue;
-			if (IsParent)
-				if (!gameObject->transform->GetParent().expired())
-					continue;
-			RunObjectIndex.push_back(gameObject);
-		}
-	}
-
-	//Messageを送る
-	{
-		//子のComponentを走らす条件
-		bool IsChild = SCENE_OPTION_UPDATE_PARENT == (option & SCENE_OPTION_UPDATE_PARENT);
-
-		for (std::shared_ptr<GameObject> gameObject : RunObjectIndex) {
-			gameObject->RunComponent(message);
-			if (IsChild)
-				//子のComponentを走らす
-				gameObject->transform->SendComponentMessageChildren(message);
-		}
-	}
+	pActiveScene.lock()->SendComponentMessage(message);
 }
 
 //	RunActiveScene_Render()
@@ -149,12 +85,140 @@ void DirectX::SceneManager::RunActiveScene(Component::Message message,BYTE optio
 //
 void DirectX::SceneManager::RunActiveScene_Render()
 {
+	pActiveScene.lock()->Render();
+}
+
+//ActiveSceneのGameObjectのColliderの更新
+void DirectX::SceneManager::ColliderUpdate()
+{
+	pActiveScene.lock()->ColliderUpdate();
+}
+
+//ActiveScene内オブジェクトのImGUI表示
+void DirectX::SceneManager::DebugGUI_ActiveScene()
+{
+	std::string label = "Scene:";
+	label += pActiveScene.lock()->GetSceneName();
+	ImGui::Begin("Scene");
+	ImGui::Text(label.c_str());
+	pActiveScene.lock()->DebugGUI();
+	ImGui::End();
+}
+
+//削除指定されたオブジェクトを削除
+void DirectX::SceneManager::CleanUp()
+{
+	pActiveScene.lock()->CleanUp();
+}
+
+//Scene遷移
+void DirectX::SceneManager::ChangeScene()
+{
+	
+	if (!IsChangeScene) return;	//遷移フラグが有効
+	//無効化
+	{
+		RunActiveScene(Component::Finalize);
+		DetachActiveScene();
+	}
+	//有効化
+	{
+		AttachActiveScene(pNextScene);
+		RunActiveScene(Component::Initialize);
+	}
+	IsChangeScene = false;
+}
+
+//ActiveSceneの取得
+std::weak_ptr<Scene> DirectX::SceneManager::GetActiveScene()
+{
+	return pActiveScene;
+}
+
+//Scene名からSceneを取得
+std::weak_ptr<Scene> DirectX::SceneManager::GetSceneByName(std::string SceneName)
+{
+	return pSceneDictionary.at(SceneName);
+}
+
+void DirectX::SceneManager::ApplyRigidbody()
+{
+	pActiveScene.lock()->ApplyRigidbody();
+}
+
+//次のSceneを設定
+void DirectX::SceneManager::SetIsChangeScene(std::weak_ptr<Scene> scene)
+{
+	pNextScene = scene;
+	IsChangeScene = true;
+}
+
+//ActiveSceneを設定
+void DirectX::SceneManager::AttachActiveScene(std::weak_ptr<Scene> scene)
+{
+	pActiveScene = scene;
+	pActiveScene.lock()->AttachActiveScene();
+}
+
+//ActiveSceneを破棄
+void DirectX::SceneManager::DetachActiveScene()
+{
+	pActiveScene.lock()->DetachActiveScene();
+	pActiveScene.reset();
+}
+
+//--- Scene -------------------------------------------------------------------
+
+//GameObjectをTag指定で追加
+GameObject* Scene::AddSceneObject(std::string name,TagName tag)
+{
+	//GameObjectの生成
+	std::shared_ptr<GameObject> object = std::shared_ptr<GameObject>(new GameObject(name,this,tag));
+	GameObjectIndex.push_back(object);
+	object->self = object;
+	object->AddComponent<Transform>();
+	return object.get();
+}
+
+void Scene::SetIsCleanUp() {
+	this->IsCleanUp = true;
+}
+
+void DirectX::Scene::SendComponentMessage(Component::Message message)
+{
+	for(auto gameObject:GameObjectIndex)
+		gameObject->RunComponent(message);
+}
+
+void DirectX::Scene::AttachActiveScene()
+{
+	this->IsLoaded = true;
+	this->IsCleanUp = false;
+	this->Load();	//読み込み
+}
+
+void DirectX::Scene::DetachActiveScene()
+{
+	this->IsLoaded = false;
+	this->IsCleanUp = false;
+	this->GameObjectIndex.clear();
+}
+
+void DirectX::Scene::CleanUp()
+{
+	if (this->IsCleanUp) return;
+	this->GameObjectIndex.remove_if([](std::shared_ptr<GameObject> gameObject) { return gameObject->IsDestroy; });
+	this->IsCleanUp = false;
+}
+
+void DirectX::Scene::Render()
+{
 	//2D描画するオブジェクトのインデックス
 	std::list<std::weak_ptr<GameObject>> RenderIndex_2D;
 
 	//3D描画
 	{
-		for (auto gameObject : pActiveScene.lock()->GameObjectIndex) {
+		for (auto gameObject :this->GameObjectIndex) {
 			//アクティブでない
 			if (!gameObject->IsActive) continue;
 			//Canvasがアタッチされている
@@ -163,7 +227,7 @@ void DirectX::SceneManager::RunActiveScene_Render()
 				continue;
 			}
 			//親がいない
-			if (gameObject->transform->GetParent().expired()){
+			if (gameObject->transform->GetParent().expired()) {
 				gameObject->RunComponent(Component::Render);
 				gameObject->transform->SendComponentMessageChildren(Component::Render);
 			}
@@ -185,224 +249,43 @@ void DirectX::SceneManager::RunActiveScene_Render()
 	}
 }
 
-//ActiveSceneのGameObjectのColliderの更新
-void DirectX::SceneManager::ColliderUpdate()
+void DirectX::Scene::ColliderUpdate()
 {
-	//GameObject
-	for(auto gameObject : pActiveScene.lock()->GameObjectIndex){
-		//Active:false
-		if (!gameObject.get()->IsActive)
-			continue;
-		//collider
-		for (auto collider : gameObject->colliders) {
-			//Enable:false
-			if (!collider.get()->GetEnable())
-				continue;
-			//otherObject
-			for(auto otherObject:pActiveScene.lock()->GameObjectIndex){
-				//OtherObject:self
-				if (gameObject == otherObject)
-					continue;
-				//Active:false
-				if (!otherObject.get()->IsActive)
-					continue;
-				//otherCollider
-				for(auto otherCollider:otherObject->colliders){
-					//Enable:false
-					if (!otherCollider.get()->GetEnable())
-						continue;
-					bool IsHit = false;
-					Vector3 Intrusion = Vector3::zero();
-
-					//Box_vs_Box
-					auto Box_Vs_Box =[gameObject,otherObject,collider,otherCollider](Vector3& Intrusion)
-					{
-						Bounds& bound1 = collider.get()->bound;
-						Bounds& bound2 = otherCollider.get()->bound;
-
-						Vector3 pos1 = gameObject->transform.get()->position() + bound1.GetCenter();
-						Vector3 pos2 = otherObject->transform.get()->position() + bound2.GetCenter();
-
-						Vector3 pos1_max = pos1 + Vector3(bound1.GetMax() * gameObject->transform->scale() * 2.0f);
-						Vector3 pos1_min = pos1 + Vector3(bound1.GetMin() * gameObject->transform->scale() * 2.0f);
-
-						Vector3 pos2_max = pos2 + Vector3(bound2.GetMax() * otherObject->transform->scale() * 2.0f);
-						Vector3 pos2_min = pos2 + Vector3(bound2.GetMin() * otherObject->transform->scale() * 2.0f);
-
-						//衝突
-						if ((pos2_min.x <= pos1_min.x && pos1_min.x <= pos2_max.x || pos2_min.x <= pos1_max.x && pos1_max.x <= pos2_max.x) &&
-							(pos2_min.y <= pos1_min.y && pos1_min.y <= pos2_max.y || pos2_min.y <= pos1_max.y && pos1_max.y <= pos2_max.y) &&
-							(pos2_min.z <= pos1_min.z && pos1_min.z <= pos2_max.z || pos2_min.z <= pos1_max.z && pos1_max.z <= pos2_max.z)
-							)
-						{
-							if (gameObject->rigidbody) {
-
-								Vector3 velocity = gameObject->rigidbody->GetVelocity();
-								Vector3 direction = pos1 - pos2;
-
-								//OtherGameObjectから見てGameObjectが下にある
-								if (direction.y < 0.0f){
-									Intrusion.y = pos1_max.y - pos2_min.y;
-									velocity.y = velocity.y > 0.0f ? 0.0f : velocity.y;
-								}
-								//OtherGameObjectから見てGameObjectが上にある
-								else{
-									Intrusion.y = pos1_min.y - pos2_max.y;
-									velocity.y = velocity.y < 0.0f ? 0.0f : velocity.y;
-								}
-
-								gameObject->rigidbody->SetVelocity(velocity);
-							}
-							return true;
-						}
-						return false;
-					};
-
-					//Box_vs_Sphere
-					auto Box_Vs_Sphere = [gameObject,otherObject,collider,otherCollider]()
-					{
-						Bounds& BoxBound = collider.get()->bound;
-						Bounds& SphBound = otherCollider.get()->bound;
-
-						Vector3 BoxPos = gameObject->transform.get()->position() + BoxBound.GetCenter();
-						Vector3 SphPos = otherObject->transform.get()->position() + SphBound.GetCenter();
-
-						Vector3 pos1_max = BoxPos + Vector3(BoxBound.GetMax() * gameObject->transform->scale());
-						Vector3 pos1_min = BoxPos + Vector3(BoxBound.GetMin() * gameObject->transform->scale());
-
-						float radius = SphBound.GetSize().x;
-
-						Vector3 posMax; 
-						Vector3 posMin; 
-
-						//X軸
-						posMax = pos1_max + Vector3(1.0f, 0.0f, 0.0f)*radius;
-						posMin = pos1_min - Vector3(1.0f, 0.0f, 0.0f)*radius;
-						if (posMin.x <= SphPos.x && SphPos.x <= posMax.x &&
-							posMin.y <= SphPos.y && SphPos.y <= posMax.y &&
-							posMin.z <= SphPos.z && SphPos.z <= posMax.z)
-						{
-							return true;
-						}
-
-						//Y軸
-						posMax = pos1_max + Vector3(0.0f, 1.0f, 0.0f) * radius;
-						posMin = pos1_min - Vector3(0.0f, 1.0f, 0.0f) * radius;
-						if (posMin.x <= SphPos.x && SphPos.x <= posMax.x &&
-							posMin.y <= SphPos.y && SphPos.y <= posMax.y &&
-							posMin.z <= SphPos.z && SphPos.z <= posMax.z)
-						{
-							return true;
-						}
-
-						//Z軸
-						posMax = pos1_max + Vector3(0.0f, 0.0f, 1.0f) * radius;
-						posMin = pos1_min - Vector3(0.0f, 0.0f, 1.0f) * radius;
-						if (posMin.x <= SphPos.x && SphPos.x <= posMax.x &&
-							posMin.y <= SphPos.y && SphPos.y <= posMax.y &&
-							posMin.z <= SphPos.z && SphPos.z <= posMax.z)
-						{
-							return true;
-						}
-						
-						Vector3 pos;
-						pos.x = SphPos.x - pos1_max.x < SphPos.x - pos1_min.x ? pos1_max.x : pos1_min.x;
-						pos.y = SphPos.y - pos1_max.y < SphPos.y - pos1_min.y ? pos1_max.y : pos1_min.y;
-						pos.z = SphPos.z - pos1_max.z < SphPos.z - pos1_min.z ? pos1_max.z : pos1_min.z;
-
-						pos = pos - SphPos;
-						if (pos.lengthSq() < radius * radius)
-							return true;
-
-						return false;
-					};
-
-					//Sphere_vs_Sphere
-					auto Sphere_Vs_Sphere =[gameObject, otherObject, collider, otherCollider](Vector3& Intrusion){
-
-						Bounds& bound1 = collider.get()->bound;
-						Bounds& bound2 = otherCollider.get()->bound;
-
-						float radius1 = bound1.GetSize().x * gameObject->transform.get()->scale().MaxElement();
-						float radius2 = bound2.GetSize().x * otherObject->transform.get()->scale().MaxElement();
-
-						Vector3 pos1 = XMVector3TransformCoord(bound1.GetCenter(), gameObject->transform.get()->WorldMatrix());
-						Vector3 pos2 = XMVector3TransformCoord(bound2.GetCenter(), otherObject->transform.get()->WorldMatrix()); //otherObject->transform.get()->position() + bound2.GetCenter();
-
-
-
-						Vector3 distance = pos1 - pos2;
-						//衝突
-						if (distance.lengthSq() < (radius1 + radius2) * (radius1 + radius2)) {
-							Intrusion = distance.normalize() * (distance.length() - (radius1 + radius2));
-							return true;
-						}
-						return false;
-					};
-
-					//Is Box
-					if(collider.get()->GetShapeType() == Collider::ShapeType::Box)
-					{
-						//Box vs Box
-						if (otherCollider.get()->GetShapeType() == Collider::ShapeType::Box)
-							IsHit = Box_Vs_Box(Intrusion);
-						//Box vs Sphere
-						else
-							IsHit = Box_Vs_Sphere();
-					}
-					//Is Sphere
-					else if(collider.get()->GetShapeType() == Collider::ShapeType::Sphere)
-					{
-						//Sphere vs Sphere
-						if (otherCollider.get()->GetShapeType() == Collider::ShapeType::Sphere)
-							IsHit = Sphere_Vs_Sphere(Intrusion);
-						else
-							IsHit = Box_Vs_Box(Intrusion);
-					}
-
-					//衝突メッセージ
-					if(IsHit){
-						//送る
-						if (collider->IsTrigger) {
-							for (auto component : gameObject->Components) {
-								component->OnTriggerEnter(otherCollider);
-							}
-						}
-						//反発
-						else if (gameObject->rigidbody) {
-							if(!otherCollider->IsTrigger)
-								gameObject->transform.get()->position(gameObject->transform.get()->position() - Intrusion);
-						}
-					}
-				}
-			}
+	for(auto gameObject:this->GameObjectIndex){
+		if (!gameObject->IsActive)continue;
+		for (auto otherObject : this->GameObjectIndex) {
+			if (gameObject == otherObject) continue;
+			if (!otherObject->IsActive) continue;
+			Collider::Hitjudgment(gameObject.get(),otherObject.get());
 		}
 	}
 }
 
-
-//ActiveScene内オブジェクトのImGUI表示
-void DirectX::SceneManager::DebugGUI_ActiveScene()
+void DirectX::Scene::ApplyRigidbody()
 {
-	std::string label = "Scene:";
-	label += pActiveScene.lock()->name;
-	ImGui::Begin("Scene");
-	ImGui::Text(label.c_str());
-	
-	for(auto gameObject : pActiveScene.lock()->GameObjectIndex)
-	{
-		ImGui::SetNextTreeNodeOpen(false,ImGuiCond_Once);
+	for(auto gameObject:this->GameObjectIndex){
+		if (!gameObject->IsActive) continue;
+		if (!gameObject->rigidbody) continue;
+		if (!gameObject->rigidbody->GetEnable()) continue;
+		gameObject->rigidbody->ApplyRigidbody();
+	}
+}
+
+void DirectX::Scene::DebugGUI()
+{
+	for (auto gameObject : this->GameObjectIndex) {
+		ImGui::SetNextTreeNodeOpen(false, ImGuiCond_Once);
 		if(ImGui::TreeNode(gameObject->name.c_str())){
 			Vector3 position = gameObject->transform.get()->position();
 			Vector3 rotation = Quaternion::ToEulerAngles(gameObject->transform.get()->rotation());
 			Quaternion q = gameObject->transform.get()->rotation();
 			Vector3 scale = gameObject->transform.get()->scale();
-			ImGui::InputFloat3("Position",&position.x);
-			ImGui::InputFloat3("Rotation",&rotation.x);
-			ImGui::InputFloat3("Scale",&scale.x);
+			ImGui::InputFloat3("Position", &position.x);
+			ImGui::InputFloat3("Rotation", &rotation.x);
+			ImGui::InputFloat3("Scale", &scale.x);
 
-			if(ImGui::TreeNode("Component")){
-				for(auto component : gameObject->Components){
+			if (ImGui::TreeNode("Component")) {
+				for (auto component : gameObject->Components) {
 					component->DebugImGui();
 				}
 				ImGui::TreePop();
@@ -410,113 +293,4 @@ void DirectX::SceneManager::DebugGUI_ActiveScene()
 			ImGui::TreePop();
 		}
 	}
-	ImGui::End();
 }
-
-//削除指定されたオブジェクトを削除
-void DirectX::SceneManager::CleanUp()
-{
-	//CeanUpしなくていい
-	if (!pActiveScene._Get()->IsCeanUp) return;
-	//削除
-	pActiveScene._Get()->GameObjectIndex.remove_if([](std::shared_ptr<GameObject> gameObject) { return gameObject->IsDestroy; });
-	//CeanUp完了
-	pActiveScene._Get()->IsCeanUp = false;
-}
-
-//Scene遷移
-void DirectX::SceneManager::ChangeScene()
-{
-	//遷移フラグが有効
-	if (!IsChangeScene) return;
-	//ActiveSceneを解除
-	DetachActiveScene();
-	//次のSceneをActiveSceneへ変更
-	AttachActiveScene(pNextScene);
-	IsChangeScene = false;
-}
-
-//ActiveSceneの取得
-std::weak_ptr<Scene> DirectX::SceneManager::GetActiveScene()
-{
-	return pActiveScene;
-}
-
-//Scene名からSceneを取得
-std::weak_ptr<Scene> DirectX::SceneManager::GetSceneByName(std::string SceneName)
-{
-	for (auto wp_Scene : pSceneIndex)
-		if (wp_Scene->name == SceneName)
-			return wp_Scene;
-	return std::weak_ptr<Scene>();
-}
-
-void DirectX::SceneManager::ApplyRigidbody()
-{
-	for(auto gameObject:pActiveScene.lock()->GameObjectIndex){
-		if (!gameObject->IsActive)
-			continue;
-		if (!gameObject->rigidbody)
-			continue;
-		if (!gameObject->rigidbody->GetEnable())
-			continue;
-		gameObject->rigidbody->ApplyRigidbody();
-	}
-}
-
-
-void DirectX::SceneManager::SetIsChangeScene(std::weak_ptr<Scene> scene)
-{
-	pNextScene = scene;
-	IsChangeScene = true;
-}
-
-//ActiveSceneを設定
-void DirectX::SceneManager::AttachActiveScene(std::weak_ptr<Scene> scene)
-{
-	pActiveScene.reset();
-	pActiveScene = scene;
-	scene.lock()->IsLoaded = true;
-	scene.lock()->IsCeanUp = false;
-	scene.lock()->Load();
-	//初期化処理
-	RunActiveScene(Component::Initialize);
-}
-
-//ActiveSceneを破棄
-void DirectX::SceneManager::DetachActiveScene()
-{
-	pActiveScene.lock()->IsLoaded = false;
-	pActiveScene.lock()->IsCeanUp = false;
-	//終了処理
-	RunActiveScene(Component::Finalize);
-	pActiveScene.lock()->GameObjectIndex.clear();
-	pActiveScene.reset();
-}
-
-//--- Scene -------------------------------------------------------------------
-
-Scene::Scene(std::string name):name(name)
-{
-	GameObjectIndex.clear();
-}
-
-Scene::~Scene()
-{
-	GameObjectIndex.clear();
-}
-
-//GameObjectをTag指定で追加
-GameObject* Scene::AddSceneObject(std::string name,TagName tag)
-{
-	//GameObjectの生成
-	std::shared_ptr<GameObject> object = std::shared_ptr<GameObject>(new GameObject(name,this,tag));
-	GameObjectIndex.push_back(object);
-	object->self = object;
-	object->AddComponent<Transform>();
-	return object.get();
-}
-
-void Scene::SetIsCeanUp(bool IsEnable) {
-	this->IsCeanUp = IsEnable;
-};
--- Engine/Module/Scene/SceneManager.h
+++ Engine/Module/Scene/SceneManager.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#define SCENE_OPTION_UPDATE_DEFAULT	(0x00)	//標準
-#define SCENE_OPTION_UPDATE_ALL		(0x01)	//例外なくすべて更新
-#define SCENE_OPTION_UPDATE_PARENT	(0x02)	//親子階層を利用した更新
-#define SCENE_OPTION_UPDATE_ACTIVE	(0x04)	//アクティブのみ更新
-
 namespace DirectX
 {
 	class Scene;
@@ -16,89 +11,113 @@ namespace DirectX
 	*/
 
 	//=== Scene管理 ===========================================================
-	class SceneManager
+	class SceneManager:public SystemManager
 	{
 	private:
-		static SceneManager* pInstance;
 		//Sceneの保管庫
-		static std::list<std::shared_ptr<Scene>> pSceneIndex;
+		static std::map<std::string, std::shared_ptr<Scene>> pSceneDictionary;
 		//現在のScene
 		static std::weak_ptr<Scene> pActiveScene;
 		//次のScene
 		static std::weak_ptr<Scene> pNextScene;
+		//Scene遷移フラグ
 		static bool IsChangeScene;
-	private:
-		SceneManager();
-		~SceneManager();
-	//--- Singleton -------------------------------------------------
 	public:
-		static void Create();			//インスタンス生成
-		static void Destroy();			//インスタンス削除
-
+		SceneManager() = default;
+		~SceneManager() = default;
+	public:
+		static void Destroy();
 	//--- Scene処理 -------------------------------------------------
 	public:
 		//Sceneの生成
-		template<typename Type>
-		static void CreateScene()
+		template<typename Type> static void CreateScene()
 		{
 			std::shared_ptr<Scene> AddScene = std::shared_ptr<Scene>(new Type());
-			pSceneIndex.push_back(AddScene);
-			AddScene->self = AddScene;
+			pSceneDictionary.emplace(AddScene->GetSceneName(),AddScene);
+			AddScene->SetSelfScene(AddScene);
 			return;
 		};
 	public:
-		//Sceneの読み込み
+		virtual void initialize();
+		virtual void update();
+		virtual void render();
+		virtual void finalize();
+	public:
 		static void LoadScene(std::string SceneName);
-		//Sceneの読み込み
-		static void LoadScene(std::weak_ptr<Scene> scene);	
-		//ActiveSceneへメッセージを送信
-		static void RunActiveScene(Component::Message message,BYTE option = SCENE_OPTION_UPDATE_DEFAULT);
-		//ActiveSceneの描画処理
+		static void LoadScene(std::weak_ptr<Scene> scene);
+		static void RunActiveScene(Component::Message message);
 		static void RunActiveScene_Render();
-		//ColliderUpdate
 		static void ColliderUpdate();
-		//ActiveScene内オブジェクトのImGUI表示
 		static void DebugGUI_ActiveScene();
-		//ActiveScene内で削除指定されたオブジェクトを削除
 		static void CleanUp();
-		//ActiveSceneとNextSceneを切り替え
 		static void ChangeScene();
-		//GetActiveScene
 		static std::weak_ptr<Scene>GetActiveScene();
-		//GetSceneByName
 		static std::weak_ptr<Scene>GetSceneByName(std::string SceneName);
 		static void ApplyRigidbody();
-
-	//--- 内部処理 --------------------------------------------------
 	private:
-		//SetIsChangeScene
 		static void SetIsChangeScene(std::weak_ptr<Scene> scene);
-		//SetActiveScene
 		static void AttachActiveScene(std::weak_ptr<Scene> scene);
-		//DitachActiveScene
 		static void DetachActiveScene();
+	};
+
+	inline void SceneManager::initialize() {};
+	inline void SceneManager::update() {
+		RunActiveScene(Component::Update);
+		RunActiveScene(Component::FixedUpdate);
+		ApplyRigidbody();
+		ColliderUpdate();
+	};
+	inline void SceneManager::render() {
+		RunActiveScene_Render();
+	};
+	inline void SceneManager::finalize() {
+		SceneManager::Destroy();
 	};
 
 	//=== Scene ==================================================================
 	class Scene
 	{
-		friend class SceneManager;
 	private:
 		const std::string name;
 		bool IsLoaded = false;
-		bool IsCeanUp = false;
+		bool IsCleanUp = false;
 	private:
-		std::weak_ptr<Scene> self;	//自身への参照
+		std::weak_ptr<Scene> self;
 		std::list<std::shared_ptr<GameObject>> GameObjectIndex;
 	protected:
 		Scene(std::string name);
 		virtual ~Scene();
 	public:
-		//Sceneが読み込まれた時
+		bool CompareName(std::string name);
 		virtual void Load() = 0;
-		//ゲームオブジェクトの生成
 		GameObject* AddSceneObject(std::string name, TagName tag);
-		//ゲームオブジェクトの削除処理を予約
-		void SetIsCeanUp(bool IsEnable);
+		void SetIsCleanUp();
+		void SendComponentMessage(Component::Message message);
+		void AttachActiveScene();
+		void DetachActiveScene();
+		void CleanUp();
+		void Render();
+		void ColliderUpdate();
+		void ApplyRigidbody();
+		void DebugGUI();
+		void SetSelfScene(std::shared_ptr<Scene> self);
+		std::string GetSceneName();
 	};
+
+	inline Scene::Scene(std::string name):name(name){
+		GameObjectIndex.clear();
+	}
+	inline Scene::~Scene(){
+		GameObjectIndex.clear();
+	}
+	inline void Scene::SetSelfScene(std::shared_ptr<Scene> self){
+		this->self = self;
+	}
+	inline bool Scene::CompareName(std::string name){
+		return this->name == name;
+	}
+	inline std::string DirectX::Scene::GetSceneName(){
+		return this->name;
+	}
+
 }
