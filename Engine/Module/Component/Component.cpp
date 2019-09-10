5c7ac8bf7875af087d7a4e50d41883a045b5ccfb
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-10 19:30:57 +0900

    Componentの更新

--- Engine/Module/Component/Component.cpp
+++ Engine/Module/Component/Component.cpp
@@ -7,24 +7,3 @@
 #include"Module\IMGUI\GUI_ImGui.h"
 
 using namespace DirectX;
-
-void Component::SetEnable(bool isEnable)
-{
-	this->IsEnable = isEnable;
-}
-
-void DirectX::Component::Run(std::weak_ptr<GameObject> gameObject, std::weak_ptr<Transform> transform,Component::Message message)
-{
-	if (!this->IsEnable) return;
-	this->gameObject = gameObject;
-	this->transform = transform;
-	this->SendBehaviourMessage(message);
-}
-
-void DirectX::Component::DebugImGui()
-{
-	if (ImGui::TreeNode(this->name.c_str())) {
-		ImGui::TreePop();
-	}
-	
-}
--- Engine/Module/Component/Component.h
+++ Engine/Module/Component/Component.h
@@ -1,16 +1,11 @@
 #pragma once
 #include<list>
+#include<memory>
 #include<map>
 
 namespace DirectX
 {
-	class GameObject;
-	class Transform;
-	class Behaviour;
-	class Collision;
-	class Collider;
-
-	/**
+	/*
 	@override
 	+ Destroy()
 	+ OnDestroy()
@@ -18,15 +13,14 @@ namespace DirectX
 	+ SendBehaviourMessage(Message message)
 	+ GetComponentName()
 	+ GetType()
-	*/
+	
 	class Component:public Object
 	{
 		friend GameObject;
 	//--- Attribute -------------------------------------------------
 	public:
 		//BehaviourMessage
-		enum Message
-		{
+		enum Message{
 			Initialize,
 			Update,
 			FixedUpdate,
@@ -68,5 +62,67 @@ namespace DirectX
 		virtual void OnCollisionEnter(std::weak_ptr<Collision> collision) {};
 		virtual void OnCollisionExit(std::weak_ptr<Collision> collision) {};
 		virtual void OnCollisionStay(std::weak_ptr<Collision> collision) {};
+	};*/
+
+	template<typename Type>
+	class Component:public IComponent
+	{
+	protected:
+		static bool IsCreate = false;
+		static ComponentID m_id = 0;
+		static std::map<EntityID, std::shared_ptr<Type>> ComponentIndex;
+	public:
+		static std::weak_ptr<Type> AddComponent(EntityID id);
+		static std::weak_ptr<Type> GetComponent(EntityID id);
+		static void DestroyComponent(EntityID id);
+		static ComponentID GetID();
+	public:
+		virtual void Run() override;
+		virtual void DebugImGui() override;
+		virtual ComponentID GetComponentID()override;
+		virtual void OnComponent();
 	};
+
+	//----------------------------------------------------------------------------
+	template<typename Type>
+	inline ComponentID Component<Type>::m_id = 0;
+
+	template<typename Type>
+	inline std::weak_ptr<Type> Component<Type>::AddComponent(EntityID id)
+	{
+		ComponentIndex.emplace(id,std::shared_ptr<Type>(new Type()));
+		return ComponentIndex.at(id);
+	}
+
+	template<typename Type>
+	inline std::weak_ptr<Type> Component<Type>::GetComponent(EntityID id)
+	{
+		return ComponentIndex.at(id);
+	}
+
+	template<typename Type> 
+	inline void Component<Type>::DestroyComponent(EntityID id)
+	{
+		ComponentIndex.erase(id);
+	}
+
+	template<typename Type>
+	inline ComponentID Component<Type>::GetID()
+	{
+		return m_id;
+	}
+	template<typename Type>
+	inline ComponentID Component<Type>::GetComponentID()
+	{
+		return this->m_id;
+	}
+
+	template<typename Type>
+	inline void Component<Type>::Run(){}
+
+	template<typename Type>
+	inline void Component<Type>::DebugImGui(){}
+
+	template<typename Type>
+	inline void Component<Type>::OnComponent(){}
 }
