b8a93b423a88c238102753975d6b17072e05015d
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-10 19:30:07 +0900

    IEntityの更新

--- Engine/Module/Entity/IEntity.h
+++ Engine/Module/Entity/IEntity.h
@@ -5,24 +5,24 @@ namespace DirectX
 	typedef unsigned int EntityID;
 	
 	//Entity Interface
-	class IEntity
+	class IEntity:public Object
 	{
 	protected:
-		const EntityID id;
+		const EntityID m_id;
 	public:
 		IEntity(EntityID id);
+		~IEntity() = default;
 		const EntityID GetEntityID();
 	};
 
 	//------------------------------------------------------------------------
 	inline IEntity::IEntity(EntityID id)
 	:
-		id(id)
+		m_id(id)
 	{
 
 	}
-	inline const EntityID IEntity::GetEntityID()
-	{
-		return id;
+	inline const EntityID IEntity::GetEntityID(){
+		return m_id;
 	}
 }
