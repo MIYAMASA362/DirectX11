e3f762d7775a516e3b3235e21e1770d81bc25a77
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-12 19:30:13 +0900

    EntityManagerの更新

--- Engine/Module/Entity/EntityManager.h
+++ Engine/Module/Entity/EntityManager.h
@@ -1,62 +1,36 @@
 #pragma once
 
-#include<map>
-#include<memory>
-#include"Module\Object\Object.h"
-#include"Module\Entity\IEntity.h"
-
 namespace DirectX
 {
-	class GameObject;
-	//--- EntityManager ----------------------------------------------------------
-	template<typename Type>
-	class EntityManager{
+	using EntityID = unsigned int;
+	class IEntity;
+
+	//=== EntityManager ==========================================================
+	class EntityManager
+	{
+		friend IEntity;
 	private:
-		EntityID m_EntityID = 0;
-		std::map<EntityID, std::shared_ptr<Type>> EntityIndex;
+		static EntityID m_EntityID;
+		static std::map<EntityID, std::weak_ptr<IEntity>> EntityIndex;
+		static EntityID AttachEntityID();
 	public:
-		void Create();
-		void Release();
+		static void Create();
+		static void Release();
 	public:
-		std::weak_ptr<Type> CreateEntity(Type* instance);
-		void RemoveEntity(EntityID id);
-		std::weak_ptr<Type> GetEntity(EntityID id);
-		EntityID AttachEntityID();
+		template<typename Type>
+		static Type* CreateEntity(Type* instance);
+		static void RemoveEntity(EntityID id);
+		static std::weak_ptr<IEntity> GetEntity(EntityID id);
 	};
 
 	//----------------------------------------------------------------------------
+	
 	template<typename Type>
-	inline void EntityManager<Type>::Create()
+	inline Type* EntityManager::CreateEntity(Type* instance)
 	{
-		EntityIndex.clear();
+		auto add = std::weak_ptr<IEntity>(instance);
+		EntityIndex.emplace(add.lock()->GetEntityID(),add);
+		return instance;
 	}
-	template<typename Type>
-	inline void EntityManager<Type>::Release()
-	{
-		EntityIndex.clear();
-	}
-	template<typename Type>
-	inline std::weak_ptr<Type> EntityManager<Type>::CreateEntity(Type* instance)
-	{
-		auto add = std::shared_ptr<Type>(instance);
-		EntityIndex.emplace(instance->GetEntityID(),add);
-		return add;
-	}
-	template<typename Type>
-	inline void EntityManager<Type>::RemoveEntity(EntityID id)
-	{
-		EntityIndex.erase(id);
-	}
-	template<typename Type>
-	inline std::weak_ptr<Type> EntityManager<Type>::GetEntity(EntityID id)
-	{
-		return EntityIndex.at(id);
-	}
-	template<typename Type>
-	inline EntityID EntityManager<Type>::AttachEntityID()
-	{
-		EntityID id = this->m_EntityID;
-		this->m_EntityID++;
-		return id;
-	}
+	
 }
