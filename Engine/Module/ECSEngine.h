d8acca73696dc6802fa796e4b9ec427cf26092c5
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-12 19:29:01 +0900

    EntityComponentSystemをまとめたHeaderを作成

---
+++ Engine/Module/ECSEngine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include"Object\Object.h"
+#include"Entity\IEntity.h"
+#include"Entity\EntityManager.h"
+#include"Entity\Entity.h"
+#include"Component\IComponent.h"
+#include"Component\ComponentManager.h"
+#include"Component\Component.h"
+#include"Behaviour\Behaviour.h"
+#include"MonoBehaviour\MonoBehaviour.h"
