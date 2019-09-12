2d596b0b4eb50c75b10e59d1adc142c312107922
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-12 19:27:10 +0900

    EntityManagerを作成

---
+++ Engine/Module/Entity/EntityManager.cpp
@@ -0,0 +1,32 @@
+#include<map>
+#include<memory>
+#include"Module\Object\Object.h"
+#include"IEntity.h"
+#include"EntityManager.h"
+
+using namespace DirectX;
+EntityID EntityManager::m_EntityID;
+std::map<EntityID, std::shared_ptr<IEntity>> EntityManager::EntityIndex;
+
+EntityID EntityManager::AttachEntityID()
+{
+	EntityID id = m_EntityID;
+	m_EntityID++;
+	return id;
+}
+void EntityManager::Create()
+{
+	EntityIndex.clear();
+}
+void EntityManager::Release()
+{
+	EntityIndex.clear();
+}
+void EntityManager::RemoveEntity(EntityID id)
+{
+	EntityIndex.erase(id);
+}
+std::weak_ptr<IEntity> EntityManager::GetEntity(EntityID id)
+{
+	return EntityIndex.at(id);
+}
