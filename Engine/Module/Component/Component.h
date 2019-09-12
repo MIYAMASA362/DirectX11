ca5a7f29858e8564b3288ed1e1498a089d6602e6
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-12 19:28:24 +0900

    Component更新

--- Engine/Module/Component/Component.h
+++ Engine/Module/Component/Component.h
@@ -1,7 +1,4 @@
 #pragma once
-#include<list>
-#include<memory>
-#include<map>
 
 namespace DirectX
 {
@@ -64,34 +61,45 @@ namespace DirectX
 		virtual void OnCollisionStay(std::weak_ptr<Collision> collision) {};
 	};*/
 
+	using ComponentID = unsigned int;
+	using EntityID = unsigned int;
+
 	template<typename Type>
 	class Component:public IComponent
 	{
 	protected:
-		static bool IsCreate = false;
-		static ComponentID m_id = 0;
+		static ComponentID m_id;
 		static std::map<EntityID, std::shared_ptr<Type>> ComponentIndex;
 	public:
-		static std::weak_ptr<Type> AddComponent(EntityID id);
-		static std::weak_ptr<Type> GetComponent(EntityID id);
+		static std::weak_ptr<Type> AddComponent(EntityID id = m_OwnerId);
+		static std::weak_ptr<Type> GetComponent(EntityID id = m_OwnerId);
 		static void DestroyComponent(EntityID id);
 		static ComponentID GetID();
+	protected:
+		const std::string name;
+	public:
+		Component(std::string name);
+		virtual ~Component();
 	public:
 		virtual void Run() override;
-		virtual void DebugImGui() override;
-		virtual ComponentID GetComponentID()override;
 		virtual void OnComponent();
+		virtual void OnDestroy() override;
+		virtual void DebugImGui() override;
+	public:
+		ComponentID GetComponentID()override final;
 	};
 
 	//----------------------------------------------------------------------------
 	template<typename Type>
-	inline ComponentID Component<Type>::m_id = 0;
+	ComponentID Component<Type>::m_id = ComponentManager::CreateComponent<Type>();
 
 	template<typename Type>
 	inline std::weak_ptr<Type> Component<Type>::AddComponent(EntityID id)
 	{
-		ComponentIndex.emplace(id,std::shared_ptr<Type>(new Type()));
-		return ComponentIndex.at(id);
+		auto add = ComponentManager::AddComponent<Type>(id);
+		add->m_OwnerId = id;
+		ComponentIndex.emplace(id,std::shared_ptr<Type>(add));
+		return add;
 	}
 
 	template<typename Type>
@@ -118,11 +126,38 @@ namespace DirectX
 	}
 
 	template<typename Type>
-	inline void Component<Type>::Run(){}
+	inline Component<Type>::Component(std::string name)
+	:
+		name(name)
+	{
+
+	}
 
 	template<typename Type>
-	inline void Component<Type>::DebugImGui(){}
+	inline Component<Type>::~Component()
+	{
+		
+	}
 
 	template<typename Type>
-	inline void Component<Type>::OnComponent(){}
+	inline void Component<Type>::Run()
+	{
+	
+	}
+
+	template<typename Type>
+	inline void Component<Type>::DebugImGui()
+	{
+		
+	}
+
+	template<typename Type>
+	inline void Component<Type>::OnComponent()
+	{
+		
+	}
+	template<typename Type>
+	inline void Component<Type>::OnDestroy()
+	{
+	}
 }
