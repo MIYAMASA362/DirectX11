1ea92356af759171f9510f69a422dbd51ec57def
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-18 18:47:08 +0900

    現在の仕様に沿わせた

--- Engine/Module/Physics/Rigidbody.cpp
+++ Engine/Module/Physics/Rigidbody.cpp
@@ -1,4 +1,18 @@
-#include"Module\Module.h"
+#include<list>
+#include<map>
+#include<memory>
+
+#include<d3d11.h>
+#include<DirectXMath.h>
+
+#include"Module\DirectX\DirectXStruct.h"
+#include"Module\DirectX\DirectX.h"
+
+#include"Module\ECSEngine.h"
+
+#include"Module\Transform\Transform.h"
+#include"Physics.h"
+#include"Rigidbody.h"
 
 DirectX::Rigidbody::Rigidbody()
 :
@@ -15,39 +29,30 @@ DirectX::Rigidbody::~Rigidbody()
 
 }
 
-void DirectX::Rigidbody::IsUseGravity(bool enable)
-{
-	this->m_useGravity = enable;
-}
-
-void DirectX::Rigidbody::SendBehaviourMessage(Component::Message message)
-{
-	switch (message)
-	{
-	case DirectX::Component::Initialize:
-		break;
-	case DirectX::Component::Update:
-		break;
-	case DirectX::Component::FixedUpdate:
-		FixedUpdate();
-		break;
-	case DirectX::Component::Render:
-		break;
-	case DirectX::Component::Finalize:
-		break;
-	default:
-		break;
-	}
-}
-
-void DirectX::Rigidbody::SetMass(float mass)
-{
-	this->m_mass = mass;
-}
+//
+//void DirectX::Rigidbody::SendBehaviourMessage(Component::Message message)
+//{
+//	switch (message)
+//	{
+//	case DirectX::Component::Initialize:
+//		break;
+//	case DirectX::Component::Update:
+//		break;
+//	case DirectX::Component::FixedUpdate:
+//		FixedUpdate();
+//		break;
+//	case DirectX::Component::Render:
+//		break;
+//	case DirectX::Component::Finalize:
+//		break;
+//	default:
+//		break;
+//	}
+//}
 
 void DirectX::Rigidbody::FixedUpdate()
 {
-	Transform* m_transform = this->transform.lock().get();
+	Transform* m_transform = this->transform().lock().get();
 
 	//d—Í
 	if(m_useGravity)
@@ -58,18 +63,8 @@ void DirectX::Rigidbody::FixedUpdate()
 		//m_velocity.y -= m_transform->position().y- (m_transform->scale().y* 0.5f) + m_velocity.y;
 }
 
-void DirectX::Rigidbody::AddForce(Vector3 force)
-{
-	this->m_velocity = this->m_mass * force;
-}
-
-const std::type_info & DirectX::Rigidbody::GetType()
-{
-	return typeid(*this);
-}
-
 void DirectX::Rigidbody::ApplyRigidbody()
 {
-	Transform* m_transform = this->transform.lock().get();
+	Transform* m_transform = this->transform().lock().get();
 	m_transform->position(m_transform->position() + this->m_velocity);
 }
--- Engine/Module/Physics/Rigidbody.h
+++ Engine/Module/Physics/Rigidbody.h
@@ -2,9 +2,8 @@
 
 namespace DirectX
 {
-	class Collision;
-
-	class Rigidbody :public Component
+	//=== Rigidbody =====================================================================
+	class Rigidbody :public Component<Rigidbody>
 	{
 	private:
 		bool m_useGravity;
@@ -15,14 +14,17 @@ namespace DirectX
 		virtual ~Rigidbody();
 	public:
 		void IsUseGravity(bool enable);
-		void SendBehaviourMessage(Component::Message message);
 		void SetMass(float mass);
 		void FixedUpdate();
 		void AddForce(Vector3 force);
-		const std::type_info& GetType()override;
 		Vector3 GetVelocity() { return m_velocity; };
 		void SetVelocity(Vector3 velocity) { m_velocity = velocity; };
 		void ApplyRigidbody();
 	};
+
+	//-----------------------------------------------------------------------------------
+	inline void Rigidbody::IsUseGravity(bool enable) { this->m_useGravity = enable; }
+	inline void Rigidbody::SetMass(float mass) { this->m_mass = mass; }
+	inline void Rigidbody::AddForce(Vector3 force) { this->m_velocity = this->m_mass * force; }
 
 }
