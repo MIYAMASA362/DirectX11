4ce6db5c9a546261d0d77297a0e7e4dec13cbda2
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-10-05 00:32:38 +0900

    Behaviour

--- Engine/Module/Behaviour/Behaviour.cpp
+++ Engine/Module/Behaviour/Behaviour.cpp
@@ -3,7 +3,9 @@
 #include<memory>
 
 #include"Module\Object\Object.h"
+#include"Module\Entity\IEntity.h"
 #include"Module\Component\IComponent.h"
 #include"Module\Component\ComponentManager.h"
 #include"Module\Component\Component.h"
 #include "Behaviour.h"
+
--- Engine/Module/Behaviour/Behaviour.h
+++ Engine/Module/Behaviour/Behaviour.h
@@ -6,30 +6,13 @@ namespace DirectX
 	template<typename Type>
 	class Behaviour:public Component<Type>
 	{
-	protected:
-		bool m_IsEnable;
 	public:
-		Behaviour(std::string name) :Component(name) {};
+		Behaviour(EntityID OwnerID,std::string name) :Component(OwnerID,name) {};
 		virtual ~Behaviour() = default;
-	public:
-		bool GetEnable();
-		void SetEnable(bool IsEnable);
 	public:
 		virtual void Run()			override {};
 		virtual void OnComponent()	override {};
 		virtual void OnDestroy()	override {};
 		virtual void DebugImGui()	override {};
 	};
-
-	//-----------------------------------------------------------------------------------
-	template<typename Type>
-	inline bool Behaviour<Type>::GetEnable()
-	{
-		return this->m_IsEnable;
-	}
-	template<typename Type>
-	inline void Behaviour<Type>::SetEnable(bool IsEnable)
-	{
-		this->m_IsEnable = IsEnable;
-	}
 }
