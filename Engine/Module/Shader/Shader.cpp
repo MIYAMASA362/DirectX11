00f23670afe48e1d13d2796dc10a085e405db00e
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-10 12:55:58 +0900

    各シェーダの設定安定化

--- Engine/Module/Shader/Shader.cpp
+++ Engine/Module/Shader/Shader.cpp
@@ -9,6 +9,10 @@
 #include"Module\Light\Light.h"
 #include"Module\Material\Material.h"
 
+
+
+//コンスタントバッファ
+
 void ConstantBuffer::CreateBuffer()
 {
 	D3D11_BUFFER_DESC bd;
@@ -54,6 +58,138 @@ void ConstantBuffer::SetPSConstantBuffer(CONSTANT_BUFFER_ELEMENT bufferElement, 
 
 
 
+//頂点シェーダ
+
+VertexShaderData::VertexShaderData()
+{
+
+}
+
+VertexShaderData::~VertexShaderData()
+{
+	if (_InputLayout) _InputLayout->Release();
+	if (_VertexShader) _VertexShader->Release();
+}
+
+void VertexShaderData::LoadShader(const char * vertexShader, VERTEX_INPUT_LAYOUT * inputLayout, unsigned int layoutSize)
+{
+	long int fsize;
+	unsigned char* buffer = nullptr;
+
+	LoadShaderFile(vertexShader, &buffer, &fsize);
+
+	D3DApp::GetDevice()->CreateVertexShader(buffer,fsize,NULL,&_VertexShader);
+
+	//入力レイアウト設定
+	{
+		D3D11_INPUT_ELEMENT_DESC* layout = new D3D11_INPUT_ELEMENT_DESC[layoutSize];
+
+		for (int i = 0; i < layoutSize; i++)
+		{
+			D3D11_INPUT_ELEMENT_DESC* target = &layout[i];
+			switch (inputLayout[i])
+			{
+			case VERTEX_INPUT_LAYOUT::VSIL_POSITION:
+			{
+				target->SemanticName = "POSITION";
+				target->SemanticIndex = 0;
+				target->Format = DXGI_FORMAT_R32G32B32_FLOAT;
+				target->InputSlot = 0;
+				target->AlignedByteOffset = 0;
+				target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+				target->InstanceDataStepRate = 0;
+			}
+			break;
+			case VERTEX_INPUT_LAYOUT::VSIL_NORMAL:
+			{
+				target->SemanticName = "NORMAL";
+				target->SemanticIndex = 0;
+				target->Format = DXGI_FORMAT_R32G32B32_FLOAT;
+				target->InputSlot = 0;
+				target->AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+				target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+				target->InstanceDataStepRate = 0;
+			}
+			break;
+			case VERTEX_INPUT_LAYOUT::VSIL_COLOR:
+			{
+				target->SemanticName = "COLOR";
+				target->SemanticIndex = 0;
+				target->Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+				target->InputSlot = 0;
+				target->AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+				target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+				target->InstanceDataStepRate = 0;
+			}
+			break;
+			case VERTEX_INPUT_LAYOUT::VSIL_TEXCOORD:
+			{
+				target->SemanticName = "TEXCOORD";
+				target->SemanticIndex = 0;
+				target->Format = DXGI_FORMAT_R32G32_FLOAT;
+				target->InputSlot = 0;
+				target->AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+				target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+				target->InstanceDataStepRate = 0;
+			}
+			break;
+			default:
+				MessageBox(NULL, "入力レイアウトの要素が欠損してます。", "エラー", MB_OK);
+				break;
+			}
+		}
+
+		D3DApp::GetDevice()->CreateInputLayout(layout, layoutSize, buffer, fsize, &_InputLayout);
+
+		delete[] layout;
+
+	}
+
+	delete[] buffer;
+}
+
+void VertexShaderData::SetShader()
+{
+	D3DApp::GetDeviceContext()->VSSetShader(_VertexShader,NULL,0);
+	D3DApp::GetDeviceContext()->IASetInputLayout(_InputLayout);
+}
+
+
+
+
+//ピクセルシェーダ
+
+PixelShaderData::PixelShaderData()
+{
+}
+
+PixelShaderData::~PixelShaderData()
+{
+	if (_PixelShader) _PixelShader->Release();
+}
+
+void PixelShaderData::LoadShader(const char * pixelShader)
+{
+	long int fsize;
+	unsigned char* buffer = nullptr;
+
+	LoadShaderFile(pixelShader, &buffer, &fsize);
+
+	D3DApp::GetDevice()->CreatePixelShader(buffer,fsize,NULL,&_PixelShader);
+
+	delete[] buffer;
+}
+
+void PixelShaderData::SetShader()
+{
+	D3DApp::GetDeviceContext()->PSSetShader(_PixelShader,NULL,0);
+}
+
+
+
+
+//シェーダ
+
 Shader::Shader()
 {
 
@@ -67,101 +203,17 @@ Shader::~Shader()
 void Shader::LoadShader(const char * VertexShader, const char * PixelShader,VERTEX_INPUT_LAYOUT* InputLayout,unsigned int LayoutSize)
 {
 	//頂点シェーダ
-	{
-		long int fsize;
-		unsigned char* buffer = nullptr;
-		LoadShaderFile(VertexShader,&buffer,&fsize);
-
-		D3DApp::GetDevice()->CreateVertexShader(buffer,fsize,NULL,&_VertexShader);
-
-		//入力レイアウト設定
-		{
-			D3D11_INPUT_ELEMENT_DESC* layout = new D3D11_INPUT_ELEMENT_DESC[LayoutSize];
-
-			for (int i = 0; i < LayoutSize; i++)
-			{
-				D3D11_INPUT_ELEMENT_DESC* target = &layout[i];
-				switch (InputLayout[i])
-				{
-				case VERTEX_INPUT_LAYOUT::VSIL_POSITION:
-				{
-					target->SemanticName = "POSITION";
-					target->SemanticIndex = 0;
-					target->Format = DXGI_FORMAT_R32G32B32_FLOAT;
-					target->InputSlot = 0;
-					target->AlignedByteOffset = 0;
-					target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-					target->InstanceDataStepRate = 0;
-				}
-				break;
-				case VERTEX_INPUT_LAYOUT::VSIL_NORMAL:
-				{
-					target->SemanticName = "NORMAL";
-					target->SemanticIndex = 0;
-					target->Format = DXGI_FORMAT_R32G32B32_FLOAT;
-					target->InputSlot = 0;
-					target->AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-					target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-					target->InstanceDataStepRate = 0;
-				}
-				break;
-				case VERTEX_INPUT_LAYOUT::VSIL_COLOR:
-				{
-					target->SemanticName = "COLOR";
-					target->SemanticIndex = 0;
-					target->Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-					target->InputSlot = 0;
-					target->AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-					target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-					target->InstanceDataStepRate = 0;
-				}
-				break;
-				case VERTEX_INPUT_LAYOUT::VSIL_TEXCOORD:
-				{
-					target->SemanticName = "TEXCOORD";
-					target->SemanticIndex = 0;
-					target->Format = DXGI_FORMAT_R32G32_FLOAT;
-					target->InputSlot = 0;
-					target->AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-					target->InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-					target->InstanceDataStepRate = 0;
-				}
-				break;
-				default:
-					MessageBox(NULL, "入力レイアウトの要素が欠損してます。", "エラー", MB_OK);
-					break;
-				}
-			}
-
-			D3DApp::GetDevice()->CreateInputLayout(layout, LayoutSize, buffer, fsize, &_VertexLayout);
-
-			delete[] layout;
-		}
-
-		delete[] buffer;
-	}
+	_VertexShader = new VertexShaderData();
+	_VertexShader->LoadShader(VertexShader,InputLayout,LayoutSize);
 
 	//ピクセルシェーダ
-	{
-		long int fsize;
-		unsigned char* buffer = nullptr;
-		LoadShaderFile(PixelShader,&buffer,&fsize);
-
-		D3DApp::GetDevice()->CreatePixelShader(buffer,fsize,NULL,&_PixelShader);
-
-		delete[] buffer;
-	}
+	_PixelShader = new PixelShaderData();
+	_PixelShader->LoadShader(PixelShader);
 
 	//定数バッファ生成
 
 	_ConstantBuffer = new ConstantBuffer();
 	_ConstantBuffer->CreateBuffer();
-
-	{
-		D3DApp::Renderer::CreateConstantBuffer<XMMATRIX>(&_WorldBuffer);
-		D3DApp::Renderer::CreateConstantBuffer<XMMATRIX>(&_ViewBuffer);
-		D3DApp::Renderer::CreateConstantBuffer<XMMATRIX>(&_ProjectionBuffer);
-	}
 
 	_Light = new Light();
 	_Material = new Material();
@@ -169,26 +221,19 @@ void Shader::LoadShader(const char * VertexShader, const char * PixelShader,VERT
 
 void Shader::Release()
 {
-	if (_Light) delete _Light;
 	if (_Material) delete _Material;
+	if (_Light) delete _Light;
 
 	if (_ConstantBuffer) delete _ConstantBuffer;
 
-	if (_ProjectionBuffer) _ProjectionBuffer->Release();
-	if (_ViewBuffer)_ViewBuffer->Release();
-	if (_WorldBuffer)_WorldBuffer->Release();
-
-	if (_VertexLayout) _VertexLayout->Release();
-	if (_VertexShader) _VertexShader->Release();
-	if (_PixelShader) _PixelShader->Release();
+	if (_PixelShader) delete _PixelShader;
+	if (_VertexShader) delete _VertexShader;
 }
 
 void Shader::SetShader()
 {
-	D3DApp::GetDeviceContext()->IASetInputLayout(_VertexLayout);
-
-	D3DApp::GetDeviceContext()->VSSetShader(_VertexShader,NULL,0);
-	D3DApp::GetDeviceContext()->PSSetShader(_PixelShader,NULL,0);
+	_VertexShader->SetShader();
+	_PixelShader->SetShader();
 
 	//SetBuffer
 	_ConstantBuffer->SetVSConstantBuffer(CONSTANT_BUFFER_WORLD,0);
@@ -201,29 +246,29 @@ void Shader::SetShader()
 
 void Shader::SetWorldMatrix(XMMATRIX * WorldMatrix)
 {
-	_WorldMatrix = *WorldMatrix;
-	_ConstantBuffer->UpdateSubresource(CONSTANT_BUFFER_WORLD,&XMMatrixTranspose(_WorldMatrix));
+	_ConstantBuffer->UpdateSubresource(CONSTANT_BUFFER_WORLD,&XMMatrixTranspose(*WorldMatrix));
 }
 
 void Shader::SetViewMatrix(XMMATRIX * ViewMatrix)
 {
-	_ViewMatrix = *ViewMatrix;
-	_ConstantBuffer->UpdateSubresource(CONSTANT_BUFFER_VIEW,&XMMatrixTranspose(_ViewMatrix));
+	_ConstantBuffer->UpdateSubresource(CONSTANT_BUFFER_VIEW,&XMMatrixTranspose(*ViewMatrix));
 }
 
 void Shader::SetProjectionMatrix(XMMATRIX * ProjectionMatrix)
 {
-	_ProjectionMatrix = *ProjectionMatrix;
-	_ConstantBuffer->UpdateSubresource(CONSTANT_BUFFER_PROJECTION,&XMMatrixTranspose(_ProjectionMatrix));
+	_ConstantBuffer->UpdateSubresource(CONSTANT_BUFFER_PROJECTION,&XMMatrixTranspose(*ProjectionMatrix));
 }
 
-void Shader::LoadShaderFile(const char * filename, unsigned char** buffer, long int * fsize)
+
+
+
+void LoadShaderFile(const char * filename, unsigned char** buffer, long int * fsize)
 {
 	FILE* file;
-	file = fopen(filename,"rb");
+	file = fopen(filename, "rb");
 	*fsize = _filelength(_fileno(file));
 
 	*buffer = new unsigned char[*fsize];
-	fread(*buffer,*fsize,1,file);
+	fread(*buffer, *fsize, 1, file);
 	fclose(file);
 }
--- Engine/Module/Shader/Shader.h
+++ Engine/Module/Shader/Shader.h
@@ -1,15 +1,9 @@
 #pragma once
 
+//前方宣言
 class Light;
 class Material;
 
-enum VERTEX_INPUT_LAYOUT
-{
-	VSIL_POSITION,
-	VSIL_NORMAL,
-	VSIL_COLOR,
-	VSIL_TEXCOORD
-};
 
 //コンスタントバッファ要素
 enum CONSTANT_BUFFER_ELEMENT
@@ -40,27 +34,59 @@ public:
 	void SetPSConstantBuffer(CONSTANT_BUFFER_ELEMENT bufferElement,UINT slot,UINT numBuffer = 1);
 };
 
+
+
+//入力レイアウト
+enum VERTEX_INPUT_LAYOUT
+{
+	VSIL_POSITION,
+	VSIL_NORMAL,
+	VSIL_COLOR,
+	VSIL_TEXCOORD
+};
+
+//頂点シェーダ
+class VertexShaderData
+{
+private:
+	ID3D11VertexShader* _VertexShader;
+	ID3D11InputLayout* _InputLayout;
+public:
+	VertexShaderData();
+	virtual~VertexShaderData();
+
+	void LoadShader(const char* vertexShader,VERTEX_INPUT_LAYOUT* inputLayout,unsigned int layoutSize);
+	void SetShader();
+};
+
+
+
+//ピクセルシェーダ
+class PixelShaderData
+{
+private:
+	ID3D11PixelShader* _PixelShader;
+public:
+	PixelShaderData();
+	virtual~PixelShaderData();
+
+	void LoadShader(const char* pixelShader);
+	void SetShader();
+};
+
+
+
 //シェーダ
 class Shader
 {
 private:
-	XMMATRIX _WorldMatrix;
-	XMMATRIX _ViewMatrix;
-	XMMATRIX _ProjectionMatrix;
-
-private:
 	Light* _Light;
 	Material* _Material;
 
-	ID3D11VertexShader* _VertexShader;
-	ID3D11PixelShader* _PixelShader;
-	ID3D11InputLayout* _VertexLayout;
-
-	ID3D11Buffer* _WorldBuffer;
-	ID3D11Buffer* _ViewBuffer;
-	ID3D11Buffer* _ProjectionBuffer;
-
 	ConstantBuffer* _ConstantBuffer;
+
+	VertexShaderData* _VertexShader;
+	PixelShaderData* _PixelShader;
 public:
 	Shader();
 	virtual~Shader();
@@ -73,8 +99,11 @@ public:
 	void SetViewMatrix(XMMATRIX* ViewMatrix);
 	void SetProjectionMatrix(XMMATRIX* ProjectionMatrix);
 
-	//filenameのファイルをバイナリ形式で読み込み,サイズなどを返します。
-	void LoadShaderFile(const char* filename,unsigned char** buffer,long int* fsize);
-
 	Material* GetMaterial() { return _Material; };
 };
+
+
+
+
+//filenameのファイルをバイナリ形式で読み込み,サイズなどを返します。
+void LoadShaderFile(const char* filename, unsigned char** buffer, long int* fsize);
