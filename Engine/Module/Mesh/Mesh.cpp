38574dd66216520ce60e7a0afff7958346278e3c
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-17 20:21:51 +0900

    Mesh.cppの追加

---
+++ Engine/Module/Mesh/Mesh.cpp
@@ -0,0 +1,41 @@
+#include<memory>
+#include<vector>
+
+#include"Module\DirectX\DirectX.h"
+#include "Mesh.h"
+
+Mesh::Mesh()
+{
+	_VertexBuffer = nullptr;
+	_IndexBuffer = nullptr;
+	
+	_VertexArray = nullptr;
+	_VertexNum = 0;
+	
+	_IndexArray = nullptr;
+	_IndexNum = 0;
+	
+	_Surface = nullptr;
+	_SurfaceNum = 0;
+}
+
+Mesh::~Mesh()
+{
+	if (_Surface) delete[] _Surface;
+	
+	if (_IndexArray) delete[] _IndexArray;
+	if (_VertexArray) delete[] _VertexArray;
+	
+	if (_IndexBuffer) _IndexBuffer->Release();
+	if (_VertexBuffer) _VertexBuffer->Release();
+}
+
+void Mesh::SetVertexBuffer()
+{
+	D3DApp::Renderer::SetVertexBuffer(_VertexBuffer, sizeof(VertexType), 0);
+}
+
+void Mesh::SetIndexBuffer()
+{
+	D3DApp::Renderer::SetIndexBuffer(_IndexBuffer);
+}
