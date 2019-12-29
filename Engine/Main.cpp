60078c38e04d866f51eaa6bb09f2e02e4edb0048
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2019-12-30 03:28:42 +0900

    Main

---
+++ Engine/Main.cpp
@@ -0,0 +1,27 @@
+#include<Windows.h>
+#include"Window\Window.h"
+
+#include"Editor\Editor.h"
+#include"Engine.h"
+
+using namespace System;
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//Window
+	Window* MainWindow = new Editor::EditorWindow();
+
+	MainWindow->GetWindowFlags() |= WindowFlags_CloseCheck;
+	MainWindow->GetWindowFlags() |= WindowFlags_DragDropFile;
+
+	MainWindow->Create(
+		hInstance,
+		"MainWindow",
+		"DirectX11 ECS",
+		1024, 576,
+		WS_OVERLAPPEDWINDOW
+	);
+
+	//MainLoop
+	return (int)MainWindow->MessageLoop();
+}
